3b0d1a82786fd45a9c7be189694517e2d1e28fce
HannanMaxwell <__max__@live.com>
2026-02-08 04:52:53 -0600

    Imported Morgan in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 import ticketRoutes from "./api/v1/routes/ticketRoutes";
 
 // Initialize Express application
 const app: Express = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/v1", ticketRoutes);
 
 // Define a route
 app.get("/", (req, res) => {
