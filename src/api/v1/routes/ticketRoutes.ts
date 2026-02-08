0ba76db461bfcf4936ef8c6635433a98fca3087e
HannanMaxwell <__max__@live.com>
2026-02-08 04:46:46 -0600

    added code to ticketRoutes.ts

---
+++ src/api/v1/routes/ticketRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+import * as ticketController from "../controllers/ticketController";
+
+const router: Router = express.Router();
+
+router.get("/tickets", ticketController.getAllTickets);
+router.get("/tickets/:id/urgency", ticketController.getTicketUrgency);
+router.post("/tickets", ticketController.createTicket);
+router.put("/tickets/:id", ticketController.updateTicket);
+router.delete("/tickets/:id", ticketController.deleteTicket);
+
+export default router;
