13f7de15d6cc5e51d1ffc908eecd7b8869e300c8
HannanMaxwell <__max__@live.com>
2026-02-08 05:34:53 -0600

    Created getAllTickets controller functinon.

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -1,5 +1,8 @@
+import { Request, Response } from "express";
+import { HTTP_STATUS } from "../../../constants/httpStatus";
+import * as ticketService from "../services/ticketService";
 
-
-
-
-
+export const getAllTickets = (req: Request, res: Response) => {
+    const tickets = ticketService.getAllTickets();
+    res.status(HTTP_STATUS.OK).json({ message: "Tickets retrieved", count: tickets.length, data: tickets });
+};
