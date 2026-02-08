0c2e543a17027803b2f1b4597a5d5b7b71a9e8fd
HannanMaxwell <__max__@live.com>
2026-02-08 05:35:32 -0600

    added getTicketUrgency controller function.

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -6,3 +6,12 @@ export const getAllTickets = (req: Request, res: Response) => {
     const tickets = ticketService.getAllTickets();
     res.status(HTTP_STATUS.OK).json({ message: "Tickets retrieved", count: tickets.length, data: tickets });
 };
+
+export const getTicketUrgency = (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const ticket = ticketService.getTicketById(id);
+    if (!ticket) return res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Ticket not found" });
+    
+    const calculated = ticketService.calculateUrgency(ticket);
+    res.status(HTTP_STATUS.OK).json({ message: "Ticket urgency calculated", data: calculated });
+};
